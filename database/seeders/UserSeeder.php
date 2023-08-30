a7c7c71c130286b814fca5ed79c4e47593f8a485
Łukasz Witkowski <witkowski@ipws.pl>
2023-08-30 06:12:42 +0200

    Added Admin middleware

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Enums\Role;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -14,10 +15,21 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->admin()->count(1)->create();
 
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        //User::factory()->admin()->count(1)->create();
+        DB::table('users')->truncate();
+        $u=[
+            "name"=> "Łukasz",
+            "email"=> "witkowski@ipws.pl",
+            "email_verified_at"=> "2023-08-30 00:43:54",
+            "password"=> '$2y$10$pGIBrJDfQtkOXXAguQzE2OjrOVIUu7nRXfwJGLctu7PnDbEl0OgH6',
+            "role"=> "admin",
+            "remember_token"=> null,
+            "created_at"=> "2023-08-30 00:41:40",
+            "updated_at"=> "2023-08-30 00:44:19",
+        ];
+        DB::table('users')->insert($u);
+
         // ]);
     }
 
--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -15,7 +15,8 @@
                 />
             </div>
             <div class="flex items-center justify-between pt-2">
-                <a class="inline-block align-baseline text-sm  hover:text-blue-800" href="{{ route('password.request') }}">Reset hasła</a>
+                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                   href="{{ route('password.request') }}"> Przypomnij hasło</a>
                 <x-form-submit>
                         <span class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                             Zaloguj się
