0f91a7ca58ae9067795eb80aad7db00064092bb9
Łukasz Witkowski <witkowski@ipws.pl>
2023-08-30 07:36:55 +0200

    Moved Password reset messaging to queue

---
+++ app/Jobs/SendPasswordResetEmailJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\PasswordResetLink;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendPasswordResetEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $email = new PasswordResetLink();
+     //   dd($this->details);
+        Mail::to('witul@skyhost.pl')->send($email);
+      //  dump($this->details);
+        //
+    }
+}
